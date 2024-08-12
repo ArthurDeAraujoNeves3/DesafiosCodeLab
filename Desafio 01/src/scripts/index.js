efb20d57fe859f47fa472f40ec0163a44f8e79e8
Arthur de Araujo Neves <arthurdearaujoneves333@gmail.com>
2024-08-12 19:11:06 -0300

    Desafio #01 concluído

---
+++ Desafio 01/src/scripts/index.js
@@ -0,0 +1,23 @@
+function curtirPublicacao(id) {
+
+    let publicacao = document.getElementById(id);
+    let icone = publicacao.children[0].children[1].children[0]; //postagem > postagemHeader > btnCurtir > icone
+    let className = publicacao.className;
+    
+    if ( className == "postagem" ) {
+
+        icone.classList.remove("fa-regular");
+        icone.classList.add("fa-solid");
+        publicacao.classList.remove("postagem");
+        publicacao.classList.add("postagemCurtida");
+
+    } else {
+
+        icone.classList.remove("fa-solid");
+        icone.classList.add("fa-regular");
+        publicacao.classList.remove("postagemCurtida");
+        publicacao.classList.add("postagem");
+
+    };
+
+};
